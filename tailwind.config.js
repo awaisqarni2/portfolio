7dad3e9fdf15d81387ef661e6c3eefc52ab36195
awaisqarni2 <awais31105@gmail.com>
2025-08-19 10:30:32 +0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"], // adjust if using React, Vue, etc.
+  theme: {
+    extend: {
+      fontFamily: {
+        Londrina: ["Londrina Outline, sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
